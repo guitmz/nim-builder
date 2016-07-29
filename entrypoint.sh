8fb657619ecaf9617661a0708fbfd41a79fa2ef0
Guilherme Thomazi Bonicontro <guitmz@users.noreply.github.com>
2016-07-29 13:20:16 -0300

    Update entrypoint.sh
    increasing verbosity, adding option to enter container shell, making binary static to overcome `glibc` and `musl libc` incompatibility and adding size optimization to binary

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 run () {
-	nim c -r --verbosity:0 $1
+	nim c -r --passL:-static --verbosity:3 $1
 }
 
 compile () {
-	nim c --verbosity:0 $1
+	nim c -d:release --passL:-static --opt:size --verbosity:3 $1
 }
 
 _sh () {
@@ -16,7 +16,7 @@ _sh () {
 usage () {
 	echo "This is a Docker container that allows you to build and run your Nim programs!"
 	echo "It's based on Alpine so it's using musl libc by default (glibc option is in the works!)"
-	echo "\nUsage:"
+	echo "Usage:"
 	echo "   run file.nim 	runs your Nim program and display it's output."
 	echo "   build file.nim 	builds your Nim program. Output binary will be located at the volume you monted to the container" 
 	echo
@@ -26,7 +26,7 @@ usage () {
 case "$1" in
     "build") compile $2;;
     "run") run $2;;
-	"sh") _sh;;
+    "sh") _sh;;
     *) usage;;
 esac
 
